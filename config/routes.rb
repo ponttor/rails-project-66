a3ec1174be78c66ab31b9ddf716006701209dec7
Alexander Illarionov <alexander.illarionov@dualbootpartners.com>
2024-07-12 22:13:52 +0200

    ref #1: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root "welcome#index"
+  scope module: :web do
+    root 'welcome#index'
 
-  resources :repositories
+    resources :repositories
 
-  post 'auth/:provider', to: 'auth#request', as: :auth_request
-  get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
-  get 'auth/destroy', to: 'auth#destroy'
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+    get 'auth/destroy', to: 'auth#destroy'
+  end
 end
