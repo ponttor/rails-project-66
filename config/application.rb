74da8d95cbff4664a66ed761fe532c342b2cd62d
Alexander Illarionov <alexander.illarionov@dualbootpartners.com>
2024-07-12 22:20:06 +0200

    ref #1: fix lint

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +20,7 @@ module RailsProject66
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    I18n.load_path += Dir["#{File.expand_path('config/locales')}/*.yml"]
+    I18n.default_locale = :ru
   end
 end
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 Sentry.init do |config|
   config.dsn = 'https://590ee78b1f0a0bee42d59d8e0effc190@o4507493639127040.ingest.de.sentry.io/4507493641289808'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
   # or
-  config.traces_sampler = lambda do |context|
+  config.traces_sampler = lambda do |_context|
     true
   end
   # Set profiles_sample_rate to profile 100%
