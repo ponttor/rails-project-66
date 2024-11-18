f42f6cea44c1ac6d28059966456526bbd5489b3a
Alexander Illarionov <alexander.illarionov@dualbootpartners.com>
2024-11-18 18:11:38 +0100

    change the token

--- app/services/github_webhook_service.rb
+++ app/services/github_webhook_service.rb
@@ -3,10 +3,12 @@
 class GithubWebhookService
   def self.create(repository_id)
     repository = Repository.find(repository_id)
-    user_token = repository.user.token
+    user_token = ENV.fetch('GITHUB_TOKEN', nil)
+    # user_token = repository.user.token
 
     client = ApplicationContainer[:octokit_client].new access_token: user_token, auto_paginate: true
     url = Rails.application.routes.url_helpers.api_checks_url
+
     client.create_hook(
       repository.github_id,
       'web',
